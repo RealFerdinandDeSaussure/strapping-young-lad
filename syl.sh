93b522ca65d7944b34b0ebbe3b0c2ee7019bdf83
Pu Anlai <inspectormustache@users.noreply.github.com>
2025-08-23 19:32:56 +0200

    Add zswap configuration options

--- syl.sh
+++ syl.sh
@@ -362,7 +362,6 @@ answer 'Yes' is given in parentheses for each question."
     done
     ask_y_n "Do you want to use a wireless connection on the system (iwd)?" && pkgs+=("iwd")
     ask_y_n "Do you want to encrypt the DNS queries made by the system (dnscrypt-proxy)?" && pkgs+=("dnscrypt-proxy")
-    ask_y_n "Will you make use of zram on the system (recommended!) (zram-generator)?" && pkgs+=("zram-generator")
     ask_y_n "Do you want to build custom (e.g. AUR) packages on the system? (base-devel)" && pkgs+=("base-devel")
     echo -n "${pkgs[*]}"
 }
